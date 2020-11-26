208c8d1e85f11514d636df8e4a1af8bd6523cc2c
TIKJA <najaroen_tik@live.com>
2020-11-26 15:37:26 +0700

    add

--- build/server.js
+++ build/server.js
@@ -1,12 +1,43 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const bodyparser = __importStar(require("body-parser"));
+const morgan_1 = __importDefault(require("morgan"));
 const member_1 = require("./api/member/member");
 const member_detail_1 = require("./api/member/member_detail");
 const app = express_1.default();
+const urlEnccodedParser = bodyparser.urlencoded({ extended: true });
+const authenticator = (req, res, next) => {
+    const username = 'admin';
+    req.user = username;
+    next();
+};
+const logger = morgan_1.default('dev');
+app.use(urlEnccodedParser);
+app.use(logger);
+app.use(authenticator);
 app.get('/', (req, res, next) => {
     res.send('Wellcome to API');
 });
--- server.ts
+++ server.ts
@@ -1,7 +1,25 @@
-import exress from 'express';
+import exress, { RequestHandler } from 'express';
+import * as bodyparser from 'body-parser'; 
+import morgan from 'morgan';
+
+
 import { apiGetMember } from './api/member/member';
 import { getMemberDetail } from './api/member/member_detail';
+import { CustomRequestHandler } from './model/express';
 const app = exress();
+const urlEnccodedParser = bodyparser.urlencoded({extended:true});
+
+const authenticator: CustomRequestHandler = (req, res, next) => {
+    const username = 'admin';
+    req.user = username;
+    next();
+}
+const logger = morgan('dev');
+
+app.use(urlEnccodedParser);
+app.use(logger);
+app.use(authenticator);
+
 
 app.get('/', (req, res, next) => {
     res.send('Wellcome to API')
